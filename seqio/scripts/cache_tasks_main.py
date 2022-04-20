c801abe160eedbfb0e578fbbb2135f680f80a201
Yilei Yang <yileiyang@google.com>
2022-04-19 21:42:13 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 442992375

--- seqio/scripts/cache_tasks_main.py
+++ seqio/scripts/cache_tasks_main.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 r"""Dumps preprocessed tasks as TFRecord of tf.Examples.
 
 Usage:
