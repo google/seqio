3e1afdb5905c5da56f98e375bdfa02de45ff0c14
Lo Ferris <loferris@google.com>
2022-05-06 13:30:00 -0700

    adding logic to conf.py

--- seqio/docs/source/conf.py
+++ seqio/docs/source/conf.py
@@ -19,6 +19,11 @@ list see the documentation:
 https://www.sphinx-doc.org/en/master/usage/configuration.html
 """
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+
 # -- Project information -----------------------------------------------------
 
 project = 'SeqIO'
