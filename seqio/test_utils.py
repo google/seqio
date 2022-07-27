438670b2655959119518ebd6ab2dea50bd6dacc2
SeqIO Team <no-reply@google.com>
2022-07-27 05:36:44 -0700

    A comment-only CL for testing purposes.
    PiperOrigin-RevId: 463563417

--- seqio/test_utils.py
+++ seqio/test_utils.py
@@ -387,6 +387,8 @@ def assert_dataset(
     expected_dtypes: Optional[Mapping[str, tf.DType]] = None):
   """Tests whether the entire dataset == expected or [expected].
 
+  Here's some extra verbiage just to see if GitHub sync is working for SeqIO.
+
   Args:
     dataset: a tf.data dataset
     expected: either a single example, or a list of examples. Each example is a
