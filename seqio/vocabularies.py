ee55959e3755996e1e38afca40a7d2abd6a5cba5
Miles Williams <42222518+mlsw@users.noreply.github.com>
2023-01-06 18:54:54 +0000

    Handle multi-byte characters in ByteVocabulary (#415)

--- seqio/vocabularies.py
+++ seqio/vocabularies.py
@@ -466,6 +466,8 @@ class ByteVocabulary(Vocabulary):
       extra_ids: an optional integer
     """
     self._byte_size = 256
+    self._byte_strings = tf.constant([
+      bytes([i]) for i in range(self._byte_size)])
     # The special tokens: 0=PAD, 1=EOS,and 2=UNK
     self._num_special_tokens = 3
     super().__init__(extra_ids=extra_ids)
@@ -577,7 +579,15 @@ class ByteVocabulary(Vocabulary):
             tf.math.greater_equal(ids, lower_bound),
             tf.math.less(ids, upper_bound)))
     ids = ids - self._num_special_tokens
-    return tf.strings.unicode_encode(ids, "UTF-8", errors="ignore")
+    string = tf.strings.reduce_join(
+      tf.gather(self._byte_strings, ids), axis=-1)
+
+    # Drop invalid byte sequences.
+    return tf.strings.unicode_transcode(
+      input=string,
+      input_encoding="UTF-8",
+      output_encoding="UTF-8",
+      errors="ignore")
 
   def __eq__(self, other):
     if not isinstance(other, ByteVocabulary):
