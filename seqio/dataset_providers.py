c24d34ebf0fa06b9e6f7fe8b4cc2478d5d27f860
SeqIO Team <no-reply@google.com>
2023-03-27 14:42:07 -0700

    # - z_code: string - contains asr hypothesis.   # - biasing: string - contains biasing context   # - transcript: string - transcript.
    PiperOrigin-RevId: 519833420

--- seqio/dataset_providers.py
+++ seqio/dataset_providers.py
@@ -1200,6 +1200,7 @@ class Task(DatasetProviderBase):
       a validated tf.data.Dataset.
     """
     actual_specs = dataset.element_spec
+    print(actual_specs)
     for feat, feat_spec in self.output_features.items():
       if feat not in actual_specs:
         if feat_spec.required:
