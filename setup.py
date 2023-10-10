07f457f248684a1ddf47920af8fd964b59d3ac27
Zhufeng Pan <panzf@google.com>
2023-10-10 13:10:27 -0700

    ping tfds to a specific version
    PiperOrigin-RevId: 572344981

--- setup.py
+++ setup.py
@@ -54,8 +54,10 @@ setuptools.setup(
         'pyglove',
         'sentencepiece',
         'tensorflow-text',
-        'tfds-nightly',
-        # remove this when sentencepiece_model_pb2 is re-generated in the
+        # Ping to a specific version to avoid endless backtracking during
+        # pip dependency resolution.
+        'tfds-nightly==4.9.2.dev202308090034',
+        # Remove this when sentencepiece_model_pb2 is re-generated in the
         # sentencepiece package.
         'protobuf<=3.20.3',
     ],
