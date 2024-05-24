980b832469e10e2b35686fa0ea11a6222b51e796
Ian Simon <iansimon@google.com>
2024-05-23 18:18:09 -0700

    remove dependency on old protobuf
    PiperOrigin-RevId: 636739772

--- setup.py
+++ setup.py
@@ -57,9 +57,6 @@ setuptools.setup(
         # Ping to a specific version to avoid endless backtracking during
         # pip dependency resolution.
         'tfds-nightly==4.9.2.dev202308090034',
-        # Remove this when sentencepiece_model_pb2 is re-generated in the
-        # sentencepiece package.
-        'protobuf<=3.20.3',
     ],
     requires_python='>=3.9',
     extras_require={
